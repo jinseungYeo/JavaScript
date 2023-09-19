da2565a6f254851452dee518451caad5a97e1334
jinseungYeo <144105054+jinseungYeo@users.noreply.github.com>
2023-09-19 11:26:04 +0900

    Add files via upload

---
+++ text3.java
@@ -0,0 +1,19 @@
+package test1;
+
+public class text3 {
+	public static void main (String args[]) {
+		try {
+			float f= Float.parseFloat(str);
+		} catch (NumberFormatException nfe) {
+			
+			f =0;
+			
+		}
+		finally {
+			System.out.println(f);
+		}
+		public static void main (String args[]) {
+			parse("korea");
+		}
+	}
+}
